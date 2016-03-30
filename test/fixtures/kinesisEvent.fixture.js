5cef36e56a3e63b36b5f1e838d53b1a20264f8c0
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-03-30 14:08:16 +0100

    add tests for emailing

--- test/fixtures/kinesisEvent.fixture.js
+++ test/fixtures/kinesisEvent.fixture.js
@@ -1,4 +1,11 @@
-var message = {
+var messageWithoutError = {
+    status: "success",
+    front: "myFront",
+    isLive: false,
+    message: "message"
+}
+
+var messageWithError = {
     status: "error",
     front: "myFront",
     isLive: false,
@@ -6,10 +13,20 @@ var message = {
 }
 
 module.exports = {
-    Records: [{
-        kinesis: {
-            data: JSON.stringify(message).toString('base64'),
-            sequenceNumber: 0
-        }
-    }]
+    withoutError: {
+        Records: [{
+            kinesis: {
+                data: JSON.stringify(messageWithoutError).toString('base64'),
+                sequenceNumber: 0
+            }
+        }]
+    },
+    withError: {
+        Records: [{
+            kinesis: {
+                data: JSON.stringify(messageWithError).toString('base64'),
+                sequenceNumber: 0
+            }
+        }]
+    }
 };
