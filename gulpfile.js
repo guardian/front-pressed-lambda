368214df6442c900f5b1e6eef13616e73d7d48ed
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-03-31 12:34:48 +0100

    add kinesis to cloudformation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var yaml = require('gulp-yaml');
+var babel = require('gulp-babel');
+var zip = require('gulp-zip');
 
 /* Cloudformation tasks */
 
@@ -16,3 +18,20 @@ gulp.task('cloudformation-dev', ['cloudformation'], function () {
     gulp.watch('cloudformation/*.yml', ['cloudformation']);
 });
 
+gulp.task('lambda-compile', function () {
+    return gulp.src('lambda/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('tmp/lambda'));
+});
+
+gulp.task('lambda-copy-deps', function () {
+    return gulp.src('node_modules/lambda-elasticsearch/index.js')
+        .pipe(gulp.dest('tmp/lambda/lambda-elasticsearch'));
+});
+
+gulp.task('lambda-archive', ['lambda-compile', 'lambda-copy-deps'], function () {
+    return gulp.src('tmp/lambda/**/*')
+        .pipe(zip('artifact.zip'))
+        .pipe(gulp.dest('tmp/riffraff/packages/lambda'))
+        .pipe(gulp.dest('tmp/riffraff/packages/curator'));
+});
