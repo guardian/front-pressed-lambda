21a20a96811f2735d94c951ea5e9cc6484ca458b
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-02-28 13:43:21 +0000

    Remove redundant gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,6 @@ var riffraff = require('node-riffraff-artefact');
 var path = require('path');
 var exec  = require('exec-chainable');
 
-/* Cloudformation tasks */
-
-gulp.task('default', ['cloudformation']);
-gulp.task('dev', ['cloudformation-dev']);
-
-gulp.task('cloudformation', function () {
-    return gulp.src('cloudformation/*.yml')
-        .pipe(yaml({ space: 4 }))
-        .pipe(gulp.dest('./tmp/riffraff/packages/cloudformation'));
-});
-
-gulp.task('cloudformation-dev', ['cloudformation'], function () {
-    gulp.watch('cloudformation/*.yml', ['cloudformation']);
-});
-
-gulp.task('riffraff', function () {
-    return gulp.src('riff-raff.yaml')
-        .pipe(yaml({ space: 4 }))
-        .pipe(gulp.dest('tmp/riffraff'));
-});
-
 gulp.task('compile', function () {
   return exec('rollup -c rollup.config.js');
 });
