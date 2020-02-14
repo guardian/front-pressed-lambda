9be0e14553fec2796595b0404ac3945c4b748245
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-02-14 17:20:48 +0000

    Reinstate get-secrets in deploy

---
+++ bin/get-secrets.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var AWS = require('aws-sdk');
+const s3 = new AWS.S3()
+
+
+var bucket = process.argv[2];
+
+s3.getObject({
+	Bucket: bucket,
+	Key: 'config.json'
+}, function (err, data) {
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	} else {
+		console.log(data.Body.toString('utf8'));
+		process.exit(0);
+	}
+});
